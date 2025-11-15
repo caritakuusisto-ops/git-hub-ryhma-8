d8022f14adb8251f3a06efdb58e15cedd1896e58
Carita K <carita.kuusisto@gmail.com>
2025-11-15 12:44:28 +0200

    Loput kommentit poistettu

--- src/App.java
+++ src/App.java
@@ -3,8 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
      Auto a1 = new Auto("Audi", "A5", 50);
-        // jos itse määritellään parametrit (rakentaja/muodostin)
-        // sulkeisiin täytyy syöttää annetut arvot
+       
      a1.merkki = "Audi";
      a1.malli = "A5";
      a1.bensanMaara = 50;
@@ -23,33 +22,26 @@ public class App {
      a2.naytaTiedot();
 
     }
-} // Appin loppu
-
-// Auto-luokka
+} 
 
 class Auto {
 
-    // Ominaisuudet, attribuutit
-    public String merkki;  // jos pyydetään määrittämään julkiseksi,
-    public String malli;   // muista public ...
+    public String merkki;  
+    public String malli;   
     public int bensanMaara;
 
-    // Itse määritelty rakentaja/muodostin (Constructor). Käytetään 
-    // this...- käskyä
     public Auto (String merkki, String malli, int bensanMaara) {
         this.merkki = merkki;
         this.malli = malli;
         this.bensanMaara = bensanMaara;
     }
 
-    // oletuksena (defaultina) käytetty rakentaja/muodostin
     public Auto() {
         merkki = "";
         malli = "";
         bensanMaara = 0;
     }
 
-    // Toiminnot, metodit
     public void jarruta() {
         System.out.println("Auto jarruttaa.");
     }
